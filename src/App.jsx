2364da677b6d91dc7abf074ab4a16a55c700ec7f
Stanley Delva <juniordelva1@gmail.com>
2025-10-09 17:43:51 -0400

    setting up basic browser paging

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import {
+    Link,
+    BrowserRouter,
+    Route,
+    Routes,
+    useParams,
+    useLocation,
+    useNavigate,
+} from "react-router-dom";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <BrowserRouter>
+                <AppContent />
+            </BrowserRouter>
+        </>
+    );
 }
 
-export default App
+function AppContent() {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    return (
+        <div className="app-content">
+            <Menu />
+
+            <div className="content-side">
+                <Routes>
+                    <Route path="/" element={<DashBoard />} />
+                    <Route path="/review" element={<Review />} />
+                    <Route path="/quiz" element={<Quiz />} />
+                </Routes>
+            </div>
+        </div>
+    );
+}
+
+function Menu() {
+    return (
+        <div className="menu-section">
+            <Link to={"/"}>
+                <h2>Dashboard</h2>
+            </Link>
+            <Link to={"/review"}>
+                <h2>Review</h2>
+            </Link>
+            <Link to={"/quiz"}>
+                <h2>Quiz</h2>
+            </Link>
+        </div>
+    );
+}
+
+function DashBoard() {
+    return (
+        <div className="dashboard-div">
+            <h1>This is the Dashboard page</h1>
+        </div>
+    );
+}
+function Review() {
+    return (
+        <div className="review-div">
+            <h1>This is the Review page</h1>
+        </div>
+    );
+}
+function Quiz() {
+    return (
+        <div className="quiz-div">
+            <h1>This is the Quiz page</h1>
+        </div>
+    );
+}
+
+export default App;
